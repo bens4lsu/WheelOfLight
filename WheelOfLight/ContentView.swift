631527023e0080b97ca4cc0de0003eab53a45b3f
Ben Schultz <bens4lsu@gmail.com>
2019-10-14 12:11:28 -0500

    Initial Commit

---
+++ WheelOfLight/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WheelOfLight
+//
+//  Created by Ben Schultz on 10/14/19.
+//  Copyright © 2019 com.concordbusinessservicesllc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
