9711fd791503be8123b1b47ce7f580699e0daeb9
Ben Schultz <bens4lsu@gmail.com>
2019-10-22 10:00:02 -0500

    starting configuration functions

---
+++ WheelOfLight/CircleConfiguration.swift
@@ -0,0 +1,20 @@
+//
+//  CircleSettings.swift
+//  WheelOfLight
+//
+//  Created by Ben Schultz on 10/22/19.
+//  Copyright © 2019 com.concordbusinessservicesllc. All rights reserved.
+//
+
+import Combine
+import SwiftUI
+
+final class CircleConfiguration: ObservableObject {
+    @Published var rotate = false
+    @Published var speed = 33.33
+    @Published var frameRatio: CGFloat = 0.7
+    @Published var bgColor: Color = .black
+    @Published var circleWidth: CGFloat = 270
+    @Published var configButtonInCenter = true
+}
+
